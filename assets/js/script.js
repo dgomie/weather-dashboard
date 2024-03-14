5ee16cdb8b3e2d666c5054f6827345431d1c73d2
Danny <danny.f.gomez@gmail.com>
2024-03-13 21:59:18 -0400

    Create functions that search lon and lat by city name, and find weather of city using lon and lat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,63 @@
-console.log("js linked")
+$(function () {
+    var searchButtonEl = $("#search")
+    
+    var apiKey = "72c7869051a25b990158d88c78993aa4"
+    var limit = "5"
+    
+    function searchCityWeather(event) {
+        event.preventDefault();
+        var cityName = $("#city-name-input").val();
+        console.log(cityName);
+
+        findCityCoords(cityName)
+        
+        clearSearch();
+    }
+
+    function findCityCoords(cityName) {
+        var coordAPIUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},US&limit=${limit}&appid=${apiKey}`
+        fetch(coordAPIUrl)
+        .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data)
+            console.log(data[0]);
+            console.log(data[0].lon)
+            console.log(data[0].lat)
+            var cityLat = data[0].lat;
+            var cityLon = data[0].lon;
+
+            findCityWeather(cityLat, cityLon);
+          });
+    }
+
+    function findCityWeather(lat, lon) {
+        var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+        var fiveDayURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+        fetch(currentWeatherURL)
+        .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data)
+          });
+
+        
+
+    }
+
+    function clearSearch() {
+     $("#city-name-input").val("");
+    };
+
+
+
+    searchButtonEl.on("click", searchCityWeather);
+    
+        
+
+
+
+});
